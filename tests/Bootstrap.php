ae4463270ccdf15f613a3c7d528af061e0b8ae05
Fabian Köstring <Fabian.Koestring@aubi-plus.de>
2016-04-22 13:21:08 +0200

    fix autoloading

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 use Zend\Loader\StandardAutoloader;
 
-if (!(@include_once __DIR__ . '/../../vendor/autoload.php') && !@(include_once __DIR__ . '/../../../autoload.php')) {
+if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !@(include_once __DIR__ . '/../../../autoload.php')) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
