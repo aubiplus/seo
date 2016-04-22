3c0f970c91fd79a354d26d90acd189dd778c9295
Fabian Köstring <Fabian.Koestring@aubi-plus.de>
2016-04-22 13:15:54 +0200

    fix autoloading

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 use Zend\Loader\StandardAutoloader;
 
+if (!(@include_once __DIR__ . '/../../vendor/autoload.php') && !@(include_once __DIR__ . '/../../../autoload.php')) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
 $loader = new StandardAutoloader();
 $loader->registerNamespace('Aubiplus\Seo', __DIR__ . '/../src/Seo');
 $loader->registerNamespace('Aubiplus\SeoTest', __DIR__ . '/SeoTest');
